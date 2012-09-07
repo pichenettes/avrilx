d659f0a311e0e5be7b50b38cd6980f0e465d9107
pichenettes <ol.gillet@gmail.com>
2012-09-07 04:25:42 +0200

    Enable slew-rate reduction by default

--- io/gpio.h
+++ io/gpio.h
@@ -88,13 +88,16 @@ SPECIALIZE_PIN(5)
 SPECIALIZE_PIN(6)
 SPECIALIZE_PIN(7)
 
-template<typename Port, uint8_t bit>
+template<typename Port, uint8_t bit, bool slow = true>
 struct Gpio {
   static inline void set_direction(PortDirection direction) {
     if (direction == INPUT) {
       Port::dir_clr(_BV(bit));
     } else {
       Port::dir_set(_BV(bit));
+      if (slow) {
+        Pin<Port, bit>::set_control(PORT_SRLEN_bm);
+      }
     }
   }
   
